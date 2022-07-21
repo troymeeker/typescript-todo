78f53950c2772f3dab7676a6377fe6af36bb2053
troymeeker <troymeeker1@gmail.com>
2022-07-21 15:44:40 -0700

    ts snowpack todo app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-import confetti from 'canvas-confetti';
+// import {v4 as uuidV4} from "uuid"
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+// console.log(
+//  uuidV4())
+
+const list = document.querySelector<HTMLUListElement>('#list')
+
+const form = document.querySelector<HTMLFormElement>('#new-task-form')
+
+const input = document.querySelector<HTMLInputElement>('#new-task-title')
+
