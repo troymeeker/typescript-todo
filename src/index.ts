af8fde9625cc6072dcb525e1a06bcb35be852cae
troymeeker <troymeeker1@gmail.com>
2022-07-25 12:27:58 -0700

    removed X for individual item removal

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,11 @@ const form = document.getElementById('new-task-form') as HTMLFormElement | null
 
 const input = document.querySelector<HTMLInputElement>('#new-task-title')
 const clear = document.querySelector<HTMLButtonElement>('#clear-all')
-const remove = document.querySelector<HTMLButtonElement>('#remove-item')
+
 
 const tasks: Task[] = loadTasks()
 tasks.forEach(addListItem)
+
 
 form?.addEventListener("submit", e => {
  e.preventDefault()
@@ -32,64 +33,60 @@ form?.addEventListener("submit", e => {
   tasks.push(newTask)
 
   addListItem(newTask)
- 
 
-input.value = ''
+  input.value = ''
 
 })
 function addListItem(task:Task){
-const item = document.createElement('li')
+  const item = document.createElement('li')
+item.addEventListener('click', () => {
 
-const checkbox = document.createElement('input')
-const button = document.createElement('button')
-button.setAttribute('id', 'remove-item')
-// item.setAttribute('class', 'task-not-complete')
+})
 
+  const checkbox = document.createElement('input')
+  // const button = document.createElement('button')
+  // button.setAttribute('id', 'remove-item')
 
-button.innerHTML = 'X'
+item.setAttribute('class', 'task-not-complete')
+// const remove = document.querySelector<HTMLButtonElement>('#remove-item') 
 
+  // button.innerHTML = 'X'
 
-checkbox.type = 'checkbox'
+  checkbox.type = 'checkbox'
 
-checkbox.addEventListener('click', () => {
+  checkbox.addEventListener('click', () => {
   task.completed = checkbox.checked
   item.classList.toggle('complete')
   // item.setAttribute('id', 'complete')
 
-  
- 
- saveTasks()
-  
+  saveTasks()  
 })
 
 // checkbox.checked = task.completed
 
-
-
-item.append(checkbox, task.title, button)
+item.append(checkbox, task.title)
 list?.append(item)
  
 }
 
-
 function saveTasks(){
-  localStorage.setItem("task", JSON.stringify(tasks))
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
 }
 
 function loadTasks(): Task[]{
-  const taskJSON = localStorage.getItem("task")
- if (taskJSON == null) return []
+  const taskJSON = localStorage.getItem("TASKS")
+  if (taskJSON == null) return []
 
- return JSON.parse(taskJSON)
+  return JSON.parse(taskJSON)
 }
 
-remove?.addEventListener('click', () => {
-     localStorage.removeItem('task')
-     console.log('remove this item only');
+// remove?.addEventListener('click', () => {
+//      localStorage.removeItem('')
+//      console.log('remove this item only');
      
-})
+// })
 
-clear?.addEventListener('click', ()=> {
+clear?.addEventListener('click', () => {
   localStorage.clear()
   
 })
